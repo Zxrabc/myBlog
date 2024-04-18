ecc42ae58701ce3eb0769ff0ef118a41ae1078d6
zxr <1961671015@qq.com>
2024-04-18 14:16:24 +0800

    sql脚本提交

---
+++ my_blog.sql
@@ -0,0 +1,30 @@
+drop database if exists db_blog;
+create database db_blog;
+use db_blog;
+
+create table tb_blog(
+    id char(16) not null comment'博客编号',
+    title varchar(32) not null comment'博客标题',
+    content text comment'博客内容',
+    createTime date not null comment'博客发布时间',
+    editTime date comment'博客最后编辑时间',
+    hot int not null comment'博客浏览量',
+    category varchar(16) not null comment'博客所属分类',
+    tags varchar(32) comment'博客标签',
+    primary key(id)
+)engine=innodb default charset=utf8mb4 comment='博客信息表';
+desc tb_blog;
+
+create table tb_tag(
+    id int not null auto_increment comment'标签编号',
+    tagName varchar(16) not null comment'标签名',
+    primary key(id)
+)engine=innodb auto_increment=1 default charset=utf8mb4 comment='标签信息表';
+desc tb_tag;
+
+create table tb_category(
+    id int not null auto_increment comment'分类编号',
+    categoryName varchar(16) not null comment'分类名',
+    primary key(id)
+)engine=innodb auto_increment=1 default charset=utf8mb4 comment='分类信息表';
+desc tb_category
